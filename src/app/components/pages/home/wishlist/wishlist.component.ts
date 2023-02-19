682dfdfd7c72a5efa8857f70845cb720849cdf73
Son Nguyen <tson2k@gmail.com>
2023-02-19 11:55:28 +0700

    wishlist page

--- src/app/components/pages/home/wishlist/wishlist.component.ts
+++ src/app/components/pages/home/wishlist/wishlist.component.ts
@@ -1,15 +1,34 @@
+import { TablePageComponent } from '#components/core/table-page/table-page.component';
+import { DatatablePagination } from '#interfaces/pagination.interface';
+import { Campaign } from '#models/campaign.model';
+import { ComponentService } from '#services/component.service';
+import { CampaignService } from '#services/http/campaign.service';
 import { Component, OnInit } from '@angular/core';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['./wishlist.component.scss']
+  styleUrls: ['./wishlist.component.scss'],
 })
-export class WishlistComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class WishlistComponent
+  extends TablePageComponent<Campaign>
+  implements OnInit
+{
+  ColumnMode = ColumnMode;
+  constructor(
+    private componentService: ComponentService,
+    private campaignService: CampaignService
+  ) {
+    super(componentService);
   }
 
+  protected requestData(): Observable<DatatablePagination<Campaign>> | any {
+    return this.campaignService.getWishlist();
+  }
+
+  redirectDetail(row: Campaign): void {
+    this.redirect(`/campaign/${row?._id}`);
+  }
 }
